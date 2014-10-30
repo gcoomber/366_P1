c4ee171b7d0ebd85081185ba38535fb8f08d7f37
George Coomber <georgecoomber@gmail.com>
2014-10-30 12:20:08 -0600

    implemented q learning

--- Qlearning.py
+++ Qlearning.py
@@ -3,31 +3,58 @@ import blackjack
 from numpy import *
 from random import *
 from scipy import *
-numEpisodes = 20
+numEpisodes = 10000
+
+# Calculate the epsilon greedy action at a given state
+def getEpsilonGreedyAction(Q, state, epsilon):
+	# epsilon must be between 0.0 and 1.0
+	assert (epsilon >= 0.0) and (epsilon <= 1.0)
+	# Choose greedy action with probability 1 - epsilon
+	action = getGreedyAction(Q, state)
+	# Choose random action with probability epsilon
+	sample = random.random_sample()
+	if sample <= epsilon:
+		action = random.randint(0,2)
+	return action
+	
+# Calculate and return the greedy action at the given state
+def getGreedyAction(Q, state):
+	# Find action at current state, that has the max action value
+	greedyAction = argmax(Q[state, :])
+	# If both actions have the same action value, choose action at random
+	if Q[state, 0] == Q[state, 1]:
+		greedyAction = random.randint(0,2)
+	return greedyAction
 
 # There are 182 different states (including start and terminal states)
 # At each state there are two possible actions 0 or 1, ie stick or hit
+N = 182
+M = 2
 
-# Create a n x m array filled with zeros, where n is the number of 
-# possible states and m is the number of possible actions at each state
-np.zeros((182, 2))
+actionStick = 0
+actionHit = 1
+
+# Create a N x M array filled with zeros, where N is the number of 
+# possible states and M is the number of possible actions at each state
+Q = zeros((N, M))
 
 returnSum = 0.0
+epsilon = 0.01
+alpha = 0.001
 for episodeNum in range(numEpisodes):
+	random.seed(episodeNum)
 	# Cumulative reward
 	G = 0
-
-	# Create a new seed for the random number generator
-	random.seed(episodeNum)
-	
 	# Init the game of blackjack and get the initial state
 	s = blackjack.init()
-	
-	# Generate a random action (0 for stick or 1 for hit) at the current state
-	# for each step of the episode. Take random action and observe the reward.
+	#Consider each step of the episode
 	while s!=-1: #-1 is terminal
-		a = random.randint(0,2)
+		# Take epsilon greedy action at each step of episode
+		a = getEpsilonGreedyAction(Q, s, epsilon)
+		#a = getGreedyAction(Q, s)
 		r,sp = blackjack.sample(s,a)
+		# Update action value function with Q-learning off-policy update
+		Q[s, a] = Q[s, a] + alpha * (r + max(Q[sp, :]) - Q[s, a])
 		G += r
 		s=sp
 	
--- randomPolicy.py
+++ randomPolicy.py
@@ -24,13 +24,10 @@ returnSum = 0.0
 for episodeNum in range(numEpisodes):
 	# Cumulative reward
 	G = 0
-
 	# Create a new seed for the random number generator
 	random.seed(episodeNum)
-	
 	# Init the game of blackjack and get the initial state
 	s = blackjack.init()
-	
 	# Generate a random action (0 for stick or 1 for hit) at the current state
 	# for each step of the episode. Take random action and observe the reward.
 	while s!=-1: #-1 is terminal
@@ -38,9 +35,7 @@ for episodeNum in range(numEpisodes):
 		r,sp = blackjack.sample(s,a)
 		G += r
 		s=sp
-	
 	print("Episode: ", episodeNum, "Return: ", G)
 	returnSum = returnSum + G
-	
 print("Average return: ", returnSum/numEpisodes)
 
