aedd17f0b63942732142182bb905a695f8543688
dwaldon <dwaldon@ualberta.ca>
2014-11-02 15:53:31 -0700

    Added command line arguments
    Makes it easier to see what number of trials results were obtained for,
    and easier to quickly run tests with different number of trials. Don't
    need to use it, but it's there if you want!

--- Experiment.py
+++ Experiment.py
@@ -1,4 +1,12 @@
 import QlearningExperiment as QLEXP
+import sys
+
+# Set the number of trials over which the average return is calculated
+# Default to 100 trials with no argument
+numTrials = 100
+if len(sys.argv) > 1:
+	# Use python QlearningExperiment <X> to run X trials
+	numTrials = int(sys.argv[1])
 
 # Initialize the best values low
 bestAlpa = 0.0
@@ -15,7 +23,7 @@ for i in range(9):
 		epsilon += 0.1
 		
 		# If return is better, update the best values
-		averageReturn = QLEXP.getLearnedReturn(epsilon,alpha,10000)
+		averageReturn = QLEXP.getLearnedReturn(epsilon,alpha,numTrials)
 		if averageReturn > bestAverageReturn:
 			bestAverageReturn = averageReturn
 			bestAlpha = alpha
